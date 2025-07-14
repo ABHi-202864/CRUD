6bf2f77b1051bee1eaaf1421f03c7c3ac2defecf
ABHI <abhi202864@gmail.com>
2025-07-15 00:34:35 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -2,3 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import ditenv from "dotenv";
+
+// PORT = 8000
+// MONGO_URL = "mongodb://localhost:27017/crudOne"
